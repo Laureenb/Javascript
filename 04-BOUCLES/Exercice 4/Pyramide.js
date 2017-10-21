3ae17718606117dfae3755f9530b3430629be40f
Laureenb <laureen_bataille@hotmail.com>
2017-10-21 14:34:03 +0200

    correction ex4

--- 04-BOUCLES/Exercice 4/Pyramide.js
+++ 04-BOUCLES/Exercice 4/Pyramide.js
@@ -1,7 +1,11 @@
 etoile="*";
 
-for (var i = 0; i <=4; i++){
+for (var i = 0; i <=4; i++){	
+	// etoile = etoile + etoile;
 
-	console.log(etoile);
-	etoile = etoile +"*";
+	var result="";
+	for (var j = 0; j <=i; j++){ //result= nombre d'etoiles par rapport a i
+		result = result + etoile;
+	}
+	console.log(result);
 }
