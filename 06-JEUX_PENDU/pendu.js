417ee049f77d65bd2b64abb60623d1c370d70b03
Laureenb <laureen_bataille@hotmail.com>
2017-10-21 11:07:14 +0200

    pendu ajout alert enter

--- 06-JEUX_PENDU/pendu.js
+++ 06-JEUX_PENDU/pendu.js
@@ -1,39 +1,59 @@
-// Utiliser la console ou les fonctions natives suivantes :
 
 //     window.prompt();
 //     window.alert();
 //     window.confirm();
 
-var mot=["P","E","N","D","U"];
-var espaces =[" "," "," "," "," "];
-var compteur=0;
+var mot =["P","E","N","D","U"];
+var espaces_rempli =[" "," "," "," "," "];
+var compteur = 0;
 
-
-function mot_a_deviner() {
-
-	var enter= prompt("Entrez une lettre en majuscule entre A et Z");
-	var enter= enter.toUpperCase();
-
-if (enter.length !=1 ) {
-	alert("Entrez seulement un caractère")
-}	
-else{
-	if(mot.indexOf(enter) == -1) {
-		alert("Introuvable!");
+var fenetre=confirm("Voulez-vous jouer au pendu?");
+	if (fenetre == false) {
+		alert("Aurevoir...");
+		if (false) {
+		fenetre.close();
+	}
 	}
 	else {
-		alert("Trouvé!");
-		for (var i = 0; i < mot.length; i++) {
-			if(enter==mot[i]);
+    	
+	function mot_a_deviner() {
+
+			var enter= prompt("Entrez une lettre en majuscule entre A et Z");
+			var enter= enter.toUpperCase();
+		
+
+		if (enter.length !=1 ) {
+			alert("Entrez seulement un caractère")
+		}	
+		else {
+			if(mot.indexOf(enter) == -1) {
+				alert("Introuvable!");
+			}
+			else {
+				alert("Trouvé!");
+			for (var i = 0; i < mot.length; i++) {
+			if(enter==mot[i]) {
+			compteur++;
+			alert("C'est la lettre de l'index" +i);
+			espaces_rempli.slice(i, 1, mot);
+			alert(espaces_rempli);
+			}
+			}
 		}
-		compteur++;
 	}
+		if (compteur == mot.length) {
+			alert("Vous avez gagné!");
+		}
+		else {
+			mot_a_deviner();
+		}
+		mot_a_deviner();
+	}
+	mot_a_deviner();
 }
 
 
 
-
-
-
-
-}
+// if (window.confirm("Une nouvelle fenêtre va s'ouvrir.")) { 
+//     window.open("fenetre.html", "Nouvelle fenêtre", "");
+// }
